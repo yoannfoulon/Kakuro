14257f4bc5bcd289c9c6cbcb94a04a6990452526
YoannFoulon <yoann.foulon@etu.univ-amu.fr>
2018-05-18 15:05:22 +0200

    ton papa

--- ContrainteDifference.hpp
+++ ContrainteDifference.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <iostream>
 #include "Contrainte.hpp"
 
 class ContrainteDifference : public Contrainte{
--- Variable.cpp
+++ Variable.cpp
@@ -32,7 +32,7 @@ void Variable::setRemovedSizes(std::vector<int> removedSizes){
 }
 
 void Variable::removeFromDomain(int value){
-	for(unsigned i = 0; i < this->getDomain().size(); ++i){
+	for(unsigned i = 0; i < this->getDomainSize(); ++i){
 		if(this->getDomain()[i] == value){
 			std::swap(this->getDomain()[i], this->getDomain()[this->getDomainSize()-1]);
 			this->setDomainSize(this->getDomainSize() - 1);
